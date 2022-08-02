1f6bc08fa1459f26b915917162b089169ae7ab2a
Marius <marius@example.com>
2022-08-02 14:00:29 +0300

    posts start

---
+++ js/posts.js
@@ -0,0 +1,12 @@
+const posts = [
+  { title: 'Post One', body: 'This is post One body' },
+  { title: 'Post Two', body: 'This is post Two body' },
+];
+
+// nusitaikom
+
+function getPosts() {
+  setTimeout(() => {
+    // sugeneruoti ir patalpinti li elemnentus is posts
+  }, 2000);
+}
