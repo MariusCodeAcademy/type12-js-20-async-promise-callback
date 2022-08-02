a7a42cf21bbd1cb030322d5f8664ff3ccf4bb234
Marius <marius@example.com>
2022-08-02 14:51:51 +0300

    promise anatomy

--- js/posts.js
+++ js/posts.js
@@ -4,9 +4,28 @@ const posts = [
 ];
 
 // nusitaikom
+const postsEl = document.getElementById('posts');
 
 function getPosts() {
   setTimeout(() => {
     // sugeneruoti ir patalpinti li elemnentus is posts
+    posts.forEach((post) => {
+      const liEl = document.createElement('li');
+      liEl.textContent = `${post.title} - ${post.body}`;
+      postsEl.append(liEl);
+    });
+  }, 1000);
+}
+
+function createPost(newPost, callBack) {
+  setTimeout(() => {
+    posts.push(newPost);
+    callBack();
   }, 2000);
 }
+
+createPost({ title: 'Post Three', body: 'This is post Three body' }, getPosts);
+
+// getPosts();
+
+// isspresti su callback
---
+++ js/promise.js
@@ -0,0 +1,20 @@
+'use strict';
+console.log('promise.js');
+
+// promise anatomy
+const pr1 = new Promise((resolve, reject) => {
+  // kazkokia logika
+  let noError = true;
+  if (noError) {
+    // jei viskas gerai darom resolve
+    resolve('OK');
+  } else {
+    // kai yra klaidu tai mes darom reject
+    reject('BAD');
+  }
+});
+console.log('pr1 ===', pr1);
+// pr1 === promise
+// resolve('OK') => .then(OK => )
+// reject('BAD'); => .catch(err => err)
+pr1.then((data) => console.log(data)).catch((err) => console.warn('something, went wrong', err));
